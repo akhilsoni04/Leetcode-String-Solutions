1c926af4cc4b83dd8e6650c44f92d67ee6709d5d
Akhil Soni <akhilsoni2611@gmail.com>
2025-08-14 22:11:37 +0530

    Leetcode 2264

---
+++ InnerLargestThreeSame.java
@@ -0,0 +1,16 @@
+class LargestThreeSame {
+    public String largestGoodInteger(String num) {
+        int i=0;
+        String max="";
+        while(i<num.length())
+        {
+            if(i+1<num.length() && num.charAt(i)==num.charAt(i+1) && i+2 < num.length() && num.charAt(i)==num.charAt(i+2))
+                if(max.compareTo(num.substring(i,i+3))<0)
+                {
+                    max=num.substring(i,i+3);
+                }
+            i++;
+        }
+       return max;
+    }
+}
